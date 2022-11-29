2eac3f2a9bf19c6f70afc28ce0989a9a97b564e9
jwillis0720 <jwillis0720@gmail.com>
2022-11-29 20:57:13 +0000

    simplified combine tests

--- tests/integration/test_combine_integration.py
+++ tests/integration/test_combine_integration.py
@@ -8,20 +8,7 @@ def test_combine(tmp_path_factory: TempdirFactory) -> None:
     path = tmp_path_factory.mktemp("test_combine")
     print(path)
     runner = CliRunner()
-    args = [
-        "combine",
-        "--verbose",
-        "-1",
-        "data/flow/flow_processed_out/fhcrc/fhcrc_manifest.csv",
-        "-2",
-        "data/flow/flow_processed_out/vrc/vrc_manifest.csv",
-        "-s",
-        "data/sequence/",
-        "-c",
-        "data/flow/collated_flow/",
-        "-o",
-        "combined_data",
-    ]
+    args = ["combine", "--verbose", "-o", str(path)]
     print("Running", "g001 combine", " ".join(args))
     result = runner.invoke(main, args)
     print(result.stdout)
